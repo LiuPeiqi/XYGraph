d6c4a62461c3c67841cf8a4c4ff0f416f9d54554
LPQ <LPQ@LPQ-PC>
2016-10-18 17:56:35 +0800

    fix y axis mark bug.

--- XYGraph/Test.cpp
+++ XYGraph/Test.cpp
@@ -10,9 +10,10 @@ using namespace cv;
 
 int main() 
 {
-	LPQ::XYGraph graph(320, 240);
+	LPQ::XYGraph graph(640, 400);
 	LPQ::XYGraph::X_AXIS_TYPE x{"10:01","10:02", "10:03", "10:04", "10:05" };
-	LPQ::XYGraph::Y_AXIS_TYPE y{ 3,1,4,2,5 };
+	LPQ::XYGraph::Y_AXIS_TYPE y{ 0.65,0.66,0.6123456,0.62,0.7456789 };
+	//LPQ::XYGraph::Y_AXIS_TYPE y{ 1.1,-2,4,-5.5,3.3 };
 	graph.Plot(x, y);
 	namedWindow("img", CV_WINDOW_AUTOSIZE);
 	imshow("img", graph.GetGraph());
--- XYGraph/XYGraph.cpp
+++ XYGraph/XYGraph.cpp
@@ -1,5 +1,6 @@
 #include <algorithm>
 #include <cassert>
+#include <cmath>
 #include "XYGraph.h"
 using namespace LPQ;
 using namespace std;
@@ -12,11 +13,14 @@ void XYGraph::Plot(const X_AXIS_TYPE & x, const Y_AXIS_TYPE & y)
 	double font_scale = 0.4;
 	int thickness = 1;
 	int base_line = 0;
-	int offset = 4;
+	int offset = 5;
 	int mark_line_length = 10;
+	int y_mark_len_limit = 6;
 	y_mark_max_size = Size(0, 0);
 	for (const auto& y_item : y) {
-		auto current_size = getTextSize(to_string(y_item), font_face, font_scale, thickness, &base_line);
+		auto text = to_string(y_item);
+		text.resize(y_mark_len_limit);
+		auto current_size = getTextSize(text, font_face, font_scale, thickness, &base_line);
 		if (y_mark_max_size.height < current_size.height) {
 			y_mark_max_size.height = current_size.height;
 		}
@@ -40,13 +44,33 @@ void XYGraph::Plot(const X_AXIS_TYPE & x, const Y_AXIS_TYPE & y)
 	auto zero = Point(left_top.x, right_bottom.y);
 	graph = Mat(height, width, CV_8UC3, Scalar::all(255));
 	rectangle(graph, left_top, right_bottom, Scalar::all(0));
-	auto y_step = (zero.y - left_top.y) / (size(y)-1);
-	Y_AXIS_TYPE y_copy(y);
-	sort(begin(y_copy), end(y_copy));
-	for (size_t i = 0; i < y_copy.size(); ++i) {
-		auto text = to_string(y_copy[i]);
+	auto y_axis_length = zero.y - left_top.y;
+	int n = y_axis_length / (y_mark_max_size.height * 3) - 1;
+	double y_min, y_max;
+	minMaxLoc(y, &y_min, &y_max);
+	double y_step = (y_max - y_min) / n;
+	if (y_step == 0) {
+		y_step = 1;
+	}
+	double decimal_place = floor(log10(abs(y_step)));
+	double step_1 = pow(10, decimal_place)*(y_step>0?1:-1);
+	double step_2 = step_1 * 2;
+	double step_5 = step_1 * 5;
+	double step = step_1;
+	if (abs(y_step) > abs(step_1)) {
+		step = step_2;
+	}
+	if (abs(y_step) > abs(step_2)) {
+		step = step_5;
+	}
+	y_min = floor(y_min / step) * step;
+	y_max = ceil(y_max / step) * step;
+	n = (y_max - y_min) / step;
+	for (size_t i = 0; i < n+1; ++i) {
+		auto text = to_string(y_min + i* step);
+		text.resize(y_mark_len_limit);
 		auto yi_size = getTextSize(text, font_face, font_scale, thickness, &base_line);
-		Point pos(zero.x - yi_size.width, zero.y - i*y_step);
+		Point pos(zero.x - yi_size.width, zero.y - static_cast<double>(i)/n * y_axis_length);
 		Point text_pos(pos.x, pos.y+yi_size.height/2);
 		putText(graph, text, text_pos, font_face, font_scale, Scalar::all(0), thickness);
 		line(graph, Point(zero.x, pos.y), Point(zero.x + mark_line_length, pos.y), Scalar::all(0), thickness);
@@ -59,8 +83,6 @@ void XYGraph::Plot(const X_AXIS_TYPE & x, const Y_AXIS_TYPE & y)
 		putText(graph, text, pos, font_face, font_scale, Scalar::all(0), thickness);
 		line(graph, Point(pos.x, zero.y), Point(pos.x, zero.y - mark_line_length), Scalar::all(0), thickness);
 	}
-	double y_min, y_max;
-	minMaxLoc(y, &y_min, &y_max);
 	auto y_normal_factor = 1/(y_max - y_min);
 	auto y_length = zero.y - left_top.y;
 	auto x_length = right_bottom.x - zero.x;
